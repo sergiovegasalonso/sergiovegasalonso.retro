169cc7f7d1604db0d0abdf29a808f4b5c933e17b
Sergio Vegas Alonso <sergio.vegas.alonso@gmail.com>
2025-10-21 23:03:10 +0200

    cv added

---
+++ src/pages/api/download-cv.ts
@@ -0,0 +1,23 @@
+import { promises as fs } from "fs";
+import path from "path";
+
+export async function GET() {
+  try {
+    const filePath = path.resolve(
+      "./src/pages/api",
+      "76022070D-SergioVegasAlonso-CV.pdf",
+    );
+    const fileBuffer = await fs.readFile(filePath);
+
+    return new Response(fileBuffer, {
+      headers: {
+        "Content-Type": "application/pdf",
+        "Content-Disposition":
+          'attachment; filename="76022070D-SergioVegasAlonso-CV.pdf"',
+      },
+    });
+  } catch (error) {
+    console.error("Error reading the PDF file:", error);
+    return new Response("Failed to download the file", { status: 500 });
+  }
+}
